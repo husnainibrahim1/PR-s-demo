62c901f8b2edbfdae8ba59007e36aef1c55afdbb
husnainibrahim1 <husnain.ibrahim@developers.studio>
2022-06-16 12:55:35 +0500

    added a script

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-cd /Users/husnainahmad/Documents/Devops/pull-request && \
+cd /Users/husnainahmad/Documents/Devops/pull-request/PR-s-demo && \
 
 if [ -z "$(git status --short)" ]; then
   echo "Clean"
 else
-  echo "$(git status --short)" > /Users/husnainahmad/Documents/Devops/pull-request/test.txt
+  echo "$(git status --short)" > /Users/husnainahmad/Documents/Devops/pull-request/PR-s-demo/test.txt
 fi
