c68ed96995b2e7cfe96b41ff6d9c95fac22fbc95
husnainibrahim1 <husnain.ibrahim@developers.studio>
2022-06-16 12:56:51 +0500

    added a script

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 cd /Users/husnainahmad/Documents/Devops/pull-request/PR-s-demo && \
 
 if [ -z "$(git status --short)" ]; then
-  echo "Clean"
+  echo "$(git pull origin master)" > /Users/husnainahmad/Documents/Devops/pull-request/PR-s-demo/test.txt
 else
   echo "$(git status --short)" > /Users/husnainahmad/Documents/Devops/pull-request/PR-s-demo/test.txt
 fi
