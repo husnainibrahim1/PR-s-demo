dfdab234159790500f2af7d016e767525071687b
husnainibrahim1 <husnain.ibrahim@developers.studio>
2022-08-30 20:03:17 +0500

    updated script

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 cd /Users/husnainahmad/Documents/Devops/pull-request/PR-s-demo && \
 
 if [ -z "$(git status --short)" ]; then
-  echo "$(git pull origin master)" > test3.txt && curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$(cat /Users/husnainahmad/Documents/Devops/pull-request/PR-s-demo/test3.txt) .\"}" https://hooks.slack.com/services/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 >> /dev/null 2>&1
+  echo "$(git pull origin master && echo "hello" && ls -la)" > test3.txt && curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$(cat /Users/husnainahmad/Documents/Devops/pull-request/PR-s-demo/test3.txt) .\"}" https://hooks.slack.com/services/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 >> /dev/null 2>&1
 else
   echo "$(git status --short)" > /Users/husnainahmad/Documents/Devops/pull-request/PR-s-demo/test.txt
 fi
